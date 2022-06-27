d7e0ff1b7de1a04512523b060f83ec867aff1f32
MarcoBeraldo <beraldomar@gmail.com>
2022-06-27 17:52:48 +0200

    added comments

--- js/script.js
+++ js/script.js
@@ -18,38 +18,58 @@
 
 // array delle immagini
 const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg',];
+
+// varibili utili
 const gallery = document.getElementById("gallery");
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
 
 for (let i = 0; i < images.length; i++) {
+    // inserisco nel div gallery tag <img> con class "item" per ogni ciclo for
     gallery.innerHTML += `<img src="${images[i]}" class="item">`
 }
 
+// definisco la variabile per prendere la posizione all'interno dell'array
 let activeCurrentIndex = 0;
+// prendo gli elementi con classe "item" all interno del div gallery
 const galleryImages = document.getElementsByClassName("item");
 console.log(galleryImages);
+
+// aggiungo la classe "active" con "display: block" agli elementi all'interno di gallery
 galleryImages[activeCurrentIndex].classList.add("active");
 
+
+// aggiungo l'eventlistener al bottone next
 nextButton.addEventListener('click', function () {
+    // rimuovo la classe "active"
     galleryImages[activeCurrentIndex].classList.remove("active");
+    // incremento il valore per prendere la prossima posiziojne nell array
     activeCurrentIndex++;
 
+    //! verifico che le immagini non siano finite e se lo sono ricomincio
     if (activeCurrentIndex >= images.length) {
         activeCurrentIndex = 0;
     }
 
+    // assegno al nuovo elemento la classe "active"
     galleryImages[activeCurrentIndex].classList.add("active");
-})
+});
 
+
+// aggiungo l'eventlistener al bottone prev
 prevButton.addEventListener('click', function () {
+    // rimuovo la classe "active"
     galleryImages[activeCurrentIndex].classList.remove("active");
+    // diminuisco il valore per prendere la posizione precedente nell' array
     activeCurrentIndex--;
 
+
+    //! quando arrivo alla prima immagine ricomincio dall'ultima
     if (activeCurrentIndex <= 0) {
         activeCurrentIndex = images.length - 1;
         console.log(activeCurrentIndex)
     }
 
+    // assegno al nuovo elemento la classe "active"
     galleryImages[activeCurrentIndex].classList.add("active");
 });
