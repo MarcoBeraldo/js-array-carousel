bb9249a4fc41ba526847e0e880a533cf245d4e16
MarcoBeraldo <beraldomar@gmail.com>
2022-06-27 15:18:59 +0200

    mark up and css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Creato un array contenente una lista di cinque immagini tra quelle fornite, creare un carosello ispirandoci alle foto in allegato.
+
+// Cerchiamo di lavorare seguendo i seguenti step in ordine:
+//     MILESTONE 1
+// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider;
+// avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+//     MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for.
+// Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme.
+// Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
+
+//     MILESTONE 3
+// Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente.
+
